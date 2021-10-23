0441686f296dba1af88327e50704a6ca5fdeb1dc
kenialo19 <3396@holbertonschool.com>
2021-10-23 12:12:15 -0500

    adding functions insertion sort, quick sort and header file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of ints in ascending order using Insertion sort algorithm
+ * @list: doubly linked list to sort
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *first = *list, *second = NULL;
+
+	if (!(*list) || !(*list)->next)
+		return;
+
+	while (first)
+	{
+		if (first->prev != NULL && first->n < first->prev->n)
+		{
+			*list = swap_list(*list, first, 1);
+			second = first;
+			first = first->next;
+
+			while (second->prev != NULL)
+			{
+				if (second->n < second->prev->n)
+				{
+					*list = swap_list(*list, second, 1);
+					continue;
+				}
+				second = second->prev;
+			}
+		}
+		first = first->next;
+	}
+}
+
+/**
+ * swap_list - Swapping function
+ * @node: Node to swap at the following direction
+ * @direction: Right is 0, Left is 1
+ * @list: list to print
+ * Return: Current node
+ */
+
+listint_t *swap_list(listint_t *list, listint_t *node, int direction)
+{
+	listint_t *aux1 = NULL, *aux2 = NULL, *temp = NULL;
+
+	if (direction == 0)
+	{
+		aux2 = node;
+		aux1 = aux2->next;
+	}
+	else
+	{
+		aux1 = node;
+		aux2 = aux1->prev;
+	}
+
+	temp = aux1->next;
+	aux1->next = aux2;
+	aux1->prev = aux2->prev;
+	if (aux2->prev != NULL)
+		aux2->prev->next = aux1;
+	else
+		list = aux1;
+	aux2->next = temp;
+	aux2->prev = aux1;
+	if (temp != NULL)
+		temp->prev = aux2;
+
+	print_list((const listint_t *) list);
+	return (list);
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,89 @@
+#include "sort.h"
+/**
+ * swap_array - function to swap two integers
+ * @a: int to swap with b
+ * @b: int to swap with a
+ * Return: nothing
+ */
+void swap_array(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * quick_sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quickSort(array, 0, size - 1, size);
+}
+
+/**
+ * quickSort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @arr: array of integers
+ * @low: Starting Index
+ * @high: Ending Index
+ * @size: size of the array
+ * Return: Nothing
+ */
+void quickSort(int arr[], int low, int high, size_t size)
+{
+	int pi = 0;
+
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now at right place */
+		pi = partition(arr, low, high, size);
+		print_array(arr, size);
+
+		/* partition and after partition */
+		quickSort(arr, low, pi - 1, size);
+		quickSort(arr, pi + 1, high, size);
+	}
+}
+
+/**
+ * partition - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @arr: array of integers
+ * @low: Starting Index
+ * @high: Ending Index
+ * @size: size of the array
+ * Return: Nothing
+ */
+int partition(int arr[], int low, int high, size_t size)
+{
+	int pivot = arr[high];
+	int i = (low - 1), j = 0;
+
+	/**
+	 * Index of smaller element and indicates the
+	 * right position of pivot found so far
+	 */
+
+	for (j = low; j <= high - 1; j++)
+	{
+		/* If current element is smaller than the pivot */
+		if (arr[j] <= pivot)
+		{
+			/* increment index of smaller element */
+			i++;
+			swap_array(&arr[i], &arr[j]);
+			/* print_array(arr, size); */
+		}
+	}
+	swap_array(&arr[i + 1], &arr[high]);
+	(void)size;
+	return (i + 1);
+}
